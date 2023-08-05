504e07b1e11021355b28958d1ddcfe39b8923003
big-charter <bigcharter@protonmail.com>
2023-08-05 17:44:31 -0400

    Script to swap; happy path

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -13,7 +13,38 @@ async function trade() {
   // get signers, set up trade and execute it
   const contract = await ethers.getContractAt("OtcSwap", contractAddress);
   const provider = ethers.provider;
-  const accounts = await ethers.getSigners();
+  const [account1, account2] = await ethers.getSigners();
+  console.log(
+    "Account1 Starting Bal:",
+    ethers.formatEther(await provider.getBalance(account1.getAddress()))
+  );
+  console.log(
+    "Account2 Starting Bal:",
+    ethers.formatEther(await provider.getBalance(account2.getAddress()))
+  );
+  const account1Contract = contract.connect(account1);
+  const account2Contract = contract.connect(account2);
+  await account1Contract.initializeTrade(account2.getAddress(), {
+    value: ethers.parseEther("1000.0"),
+  });
+  await account2Contract.joinTrade(account1.getAddress(), {
+    value: ethers.parseEther("3000.0"),
+  });
+  console.log("Account1 Prop:", await account1Contract.getProp());
+  console.log("Account2 Prop:", await account2Contract.getProp());
+  await account1Contract.approve();
+  await account2Contract.approve();
+  console.log("Both accounts have approved");
+  await account1Contract.executeSwap();
+  console.log("Swap executed");
+  console.log(
+    "Account1 Ending Bal:",
+    ethers.formatEther(await provider.getBalance(account1.getAddress()))
+  );
+  console.log(
+    "Account2 Endering Bal:",
+    ethers.formatEther(await provider.getBalance(account2.getAddress()))
+  );
 }
 
 deploy()
